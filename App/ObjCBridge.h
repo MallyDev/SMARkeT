eb720a4eae19404e3f0b4a0104aae6c9fe4e979a
Francesco Caposiena <fcaposiena@sal-01.lan>
2017-04-01 16:06:04 +0200

    Aggiunta Estimote SDK, Main Storyboard del vecchio e OBJbridge

---
+++ App/ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  App
+//
+//  Created by Francesco Caposiena on 01/04/2017.
+//  Copyright © 2017 Francesco Caposiena. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+#import <EstimoteSDK/EstimoteSDK.h>
+
+
+#endif /* ObjCBridge_h */
