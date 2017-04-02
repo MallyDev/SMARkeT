203c84de9f1be5b5f0fdb6d61bbd23838109e32d
Marco Capozzo <marcocapozzo@sal-29.local>
2017-04-02 12:19:58 +0200

    A

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -61,6 +61,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, ESTBeaconManagerDelegate 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.beaconManager.delegate = self
         return true
     }
 
