2d800cd0674adc19328efee7d6b9ac8c969fece0
Francesco Caposiena <fcaposiena@10-24-48-29.0200.sal.it.iosda.org>
2017-04-03 17:49:16 +0200

    Modifica Shopping list table view

--- App/ShoppingListTableViewController.swift
+++ App/ShoppingListTableViewController.swift
@@ -14,6 +14,11 @@ class ShoppingListTableViewController: UITableViewController {
     var list : Array<Product> = []
     
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        list = PersistenceManager.fetchList()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,7 +27,7 @@ class ShoppingListTableViewController: UITableViewController {
         
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        list = PersistenceManager.fetchList()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -45,7 +50,7 @@ class ShoppingListTableViewController: UITableViewController {
     
      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingListCell", for: indexPath) as!ShoppingListTableViewCell
-     
+        
         // Configure the cell...
         cell.nameLabel.text = list[indexPath.row].name!
         cell.departmentLabel.text = list[indexPath.row].department
