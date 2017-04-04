dea56e79653edad171ed7fc6bd09e2df66600eec
Ivan Colucci <icolucci2@10-24-48-28.0200.sal.it.iosda.org>
2017-04-04 09:45:44 +0200

    Item detail funzionante

--- App/ShoppingListTableViewController.swift
+++ App/ShoppingListTableViewController.swift
@@ -135,9 +135,11 @@ class ShoppingListTableViewController: UITableViewController {
         navigationItem.backBarButtonItem = backItem // This will show in the next view controller being pushed
 
         if segue.identifier == "showItem" {
+            print("i'm in!")
             let currentRow = tableView.indexPathForSelectedRow?.row
             let currentItem = list[currentRow!]
             let dstView = segue.destination as! ItemDetailViewController
+            dstView.title = currentItem.name!
             dstView.item = currentItem
         }
     }
