367ed9e5d5d3d0da0801da7d4f7ced42e19c5c15
Ivan Colucci <icolucci2@10-24-48-28.0200.sal.it.iosda.org>
2017-04-04 11:13:51 +0200

    funzionante

--- App/OffersTabViewController.swift
+++ App/OffersTabViewController.swift
@@ -35,15 +35,22 @@ class OffersTabViewController: UIViewController, UITableViewDataSource, UITableV
     }
     
     
-    /*
+     /*
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destinationViewController.
      // Pass the selected object to the new view controller.
-     }
-     */
+        if segue.identifier == "showItem" {
+            let currentRow = tableView.indexPathForSelectedRow?.row
+            let currentItem = favourites[currentRow!]
+            let dstView = segue.destination as! ItemDetailViewController
+            dstView.title = currentItem.name!
+            dstView.item = currentItem
+        }
+     }*/
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! OfferCellTableViewCell
